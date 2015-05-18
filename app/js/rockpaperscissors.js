bdd798dd3fd345bb706a1e01efeb5f378529dba0
Jerry Pallath <jpallath@nyu.edu>
2015-05-17 22:38:16 -0400

    attempted to add to lines of code to git the third time

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,18 +22,37 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move==="scissors"||"Scissors"){
+        console.log("scissors")
+    }
+    else if (move==="rock"||"Rock"){
+        console.log("rock")
+    } 
+    else if (move==="paper"||"Paper"){
+        console.log("paper")
+    }
+    else {
+        getInput()
+    }
+    return move;
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    if (move==="scissors"||"Scissors"){
+        console.log("scissors")
+    }
+    else if (move==="rock"||"Rock"){
+        console.log("rock")
+    } 
+    else if (move==="paper"||"Paper"){
+        console.log("paper")
+    }
+    else {
+        randomPlay()
+    }
+    return move;
 }
+
 
 function getWinner(playerMove,computerMove) {
     var winner;
